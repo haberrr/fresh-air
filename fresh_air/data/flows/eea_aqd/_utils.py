00067b0bb3ef332201f9cba6c272cb5ac59e3ca1
Andrey Pavlov <dron.pavlov@gmail.com>
2023-01-22 16:39:47 +0100

    doc flows.eea_aqd._utils: add docstring

--- fresh_air/data/flows/eea_aqd/_utils.py
+++ fresh_air/data/flows/eea_aqd/_utils.py
@@ -1,4 +1,5 @@
 def _url_parse(part: int):
+    """Retrieve `part`-th part of the URL when split by '/'."""
     def wrapped(col_name: str):
         return lambda x: x[col_name].str.split('/').str[part]
 
